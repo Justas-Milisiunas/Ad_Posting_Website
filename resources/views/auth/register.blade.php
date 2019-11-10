4fdc1934160a964e5d80810c7e9c6374d4b51cf2
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-10 12:26:55 +0200

    Fixed issue with user's mobile number not loading from the database.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,12 +40,12 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Telefono numeris') }}</label>
+                            <label for="mobile_number" class="col-md-4 col-form-label text-md-right">{{ __('Telefono numeris') }}</label>
 
                             <div class="col-md-6">
-                                <input id="mobile-number" type="tel" class="form-control @error('tel') is-invalid @enderror" name="mobile-number" value="{{ old('mobile-number') }}" required autocomplete="mobile-number">
+                                <input id="mobile_number" type="tel" class="form-control @error('tel') is-invalid @enderror" name="mobile_number" value="{{ old('mobile_number') }}" required autocomplete="mobile_number">
 
-                                @error('mobile-number')
+                                @error('mobile_number')
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
