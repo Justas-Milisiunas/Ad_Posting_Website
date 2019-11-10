795ed34b2921662547eb526369c5cf93efbc9f04
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-10 09:46:43 +0200

    Added mobile phone to user's model

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,6 +40,20 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Telefono numeris') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="mobile-number" type="tel" class="form-control @error('tel') is-invalid @enderror" name="mobile-number" value="{{ old('mobile-number') }}" required autocomplete="mobile-number">
+
+                                @error('mobile-number')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Slaptažodis') }}</label>
 
                             <div class="col-md-6">
