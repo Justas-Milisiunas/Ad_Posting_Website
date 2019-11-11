bb6f3f174082a47705a27b2ca95dda1b57a145c4
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-11 14:59:42 +0200

    Created main page

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -11,6 +11,9 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">Titulinis</a>
+                </li>
                 @if (isset(Auth::user()->id) && Auth::user()->id == 2)
                     <li class="nav-item">
                         <a class="nav-link" href="/users">Visi Vartotojai</a>
