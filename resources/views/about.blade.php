172b8d1ac0135816338a91168f17f364c39d35af
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-11 14:58:14 +0200

    Created main page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-text">Titulinis puslapis</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">Tema 1 ag_b: Internetinių skebimų sistema</li>
+                <li class="list-group-item">Darė Justas Milišiūnas</li>
+            </ul>
+        </div>
+    </div>
+@stop
